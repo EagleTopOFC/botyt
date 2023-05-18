e9811ea05f4c4b591a9878a3c8e135b3cd7a3d5c
EagleTopOFC <116709074+EagleTopOFC@users.noreply.github.com>
2023-05-18 07:07:55 -0600

    Create bot_yt.py

---
+++ bot_yt.py
@@ -0,0 +1,29 @@
+from selenium import webdriver
+
+import time
+
+url = 'https://youtu.be/ROynRDePgT4'  # URL del sitio web a visitar
+
+num_visitas = 50  # Número de visitas a generar
+
+tiempo_visita = 60  # Tiempo en segundos para permanecer en la página
+
+# Configuración del navegador
+
+options = webdriver.ChromeOptions()
+
+options.add_argument('--headless')  # Ejecución en segundo plano, sin ventana del navegador
+
+driver = webdriver.Chrome(options=options)
+
+for _ in range(num_visitas):
+
+    driver.get(url)
+
+    print(f'Visita iniciada: {driver.current_url}')
+
+    time.sleep(tiempo_visita)
+
+    print(f'Visita completada: {driver.current_url}')
+
+driver.quit()  # Cerrar el navegador al finalizar
